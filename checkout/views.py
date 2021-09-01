5d537f8b8a3ef6ee276339709dd7e66db14528aa
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-01 07:19:52 +0100

    Fixing the image field

--- checkout/views.py
+++ checkout/views.py
@@ -94,8 +94,7 @@ def checkout(request):
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(
-                request, "There's nothing in your bag at the moment")
+            messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
