5509c4ebdc0427b7a96a0a3f26f70afbd89f9921
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-01 20:47:32 +0100

    Disable colestatic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -200,3 +200,5 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+
